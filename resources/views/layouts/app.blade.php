7c11d24b45205826622596891fea8e5479719d44
fari72 <fariaffrizal@gmail.com>
2023-01-31 07:43:06 +0700

    menambahkan file css dan js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,23 @@
   <link rel="stylesheet" href="{{asset('assets/modules/fontawesome/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/modules/izitoast/css/iziToast.min.css')}}">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <!-- CSS Libraries -->
+{{-- DataTables --}}
+  <link rel="stylesheet" href="{{ asset('assets/datatable/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/datatable/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/datatable/buttons.bootstrap4.min.css') }}">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
   <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+
+   <!-- Template iziToast -->
+ <link rel="stylesheet" href="{{ asset('assets/modules/izitoast/css/iziToast.min.css') }}">
+
 <!-- Start GA -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
 
@@ -46,6 +58,16 @@
   <script src="{{asset('assets/modules/moment.min.js')}}"></script>
   <script src="{{asset('assets/js/stisla.js')}}"></script>
   
+   {{-- DataTables --}}
+   <script src="{{ asset('assets/datatable/jquery.dataTables.min.js') }}"></script>
+   <script src="{{ asset('assets/datatable/dataTables.bootstrap4.min.js') }}"></script>
+   <script src="{{ asset('assets/datatable/dataTables.responsive.min.js') }}"></script>
+   <script src="{{ asset('assets/datatable/responsive.bootstrap4.min.js') }}"></script>
+
+   {{-- SweetAlert --}}
+   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+   <!-- JS Izitoast -->
  <script src="{{asset('assets/modules/izitoast/js/iziToast.min.js')}}"></script>
   
   <!-- JS Libraies -->
