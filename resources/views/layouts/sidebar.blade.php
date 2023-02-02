a4e24e5d9735a61db1bf5ac51ccbe0ee96c032fa
fari72 <fariaffrizal@gmail.com>
2023-02-02 11:45:26 +0700

    pembagian admin,kasir,owner  melihat menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,8 +5,9 @@
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="{{route('dashboard.index')}}">Lau</a>
-    </div>
+    </div> 
     <ul class="sidebar-menu">
+      @if(auth()->user()->role == 'admin')
       <li class="menu-header">Dashboard</li>
       <li class="{{ active('dashboard*') }}"><a class="nav-link" href="/dashboard"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>           
       <li class="menu-header">User</li>
@@ -22,6 +23,21 @@
       <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a></li>           
       <li class="menu-header">Laporan</li>           
       <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>           
+      @endif
+
+      @if(auth()->user()->role == 'kasir')
+      <li class="menu-header">Member</li>           
+      <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="fa-solid fa-users"></i></i><span>Member</span></a></li>
+      <li class="menu-header">Transaksi</li>           
+      <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a></li>
+      <li class="menu-header">Laporan</li>           
+      <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>
+      @endif
+
+      @if(auth()->user()->role == 'owner')
+      <li class="menu-header">Laporan</li>           
+      <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>
+      @endif
     </ul>
   </aside>
 </div>
