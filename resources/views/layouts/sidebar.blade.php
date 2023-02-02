984a42b0ce1e665b4d39a367c15a21155295bed9
fari72 <fariaffrizal@gmail.com>
2023-02-02 10:47:50 +0700

    menambahkan agar bisa melihat menu pada aplikasi

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,7 +10,17 @@
       <li class="menu-header">Dashboard</li>
       <li class="{{ active('dashboard*') }}"><a class="nav-link" href="/dashboard"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>           
       <li class="menu-header">User</li>
-      <li class="{{ active('user*') }}"><a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-user"></i><span>User</span></a></li>           
+      <li class="{{ active('user*') }}"><a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-user"></i><span>User</span></a></li>
+      <li class="menu-header">Outlet</li>
+      <li class="{{ active('outlet*') }}"><a class="nav-link" href="{{route('outlet.index')}}"><i class="#"></i><span>Outlet</span></a></li>           
+      <li class="menu-header">Member</li>           
+      <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="#"></i><span>Member</span></a></li>           
+      <li class="menu-header">Paket</li>           
+      <li class="{{ active('paket*') }}"><a class="nav-link" href="{{route('paket.index')}}"><i class="#"></i><span>Paket</span></a></li>           
+      <li class="menu-header">Transaksi</li>           
+      <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="#"></i><span>Transaksi</span></a></li>           
+      <li class="menu-header">Laporan</li>           
+      <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="#"></i><span>Laporan</span></a></li>           
     </ul>
   </aside>
 </div>
