bda74aed3cfa923f4a692d5c81d30ad4f9cd1ac0
fari72 <fariaffrizal@gmail.com>
2023-02-02 11:11:15 +0700

    icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,15 +12,16 @@
       <li class="menu-header">User</li>
       <li class="{{ active('user*') }}"><a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-user"></i><span>User</span></a></li>
       <li class="menu-header">Outlet</li>
-      <li class="{{ active('outlet*') }}"><a class="nav-link" href="{{route('outlet.index')}}"><i class="#"></i><span>Outlet</span></a></li>           
+      <li class="{{ active('outlet*') }}"><a class="nav-link" href="{{route('outlet.index')}}"><i class="fa-solid fa-store"></i><span>Outlet</span></a></li>           
       <li class="menu-header">Member</li>           
-      <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="#"></i><span>Member</span></a></li>           
+      <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="fa-solid fa-users"></i></i><span>Member</span></a></li>           
       <li class="menu-header">Paket</li>           
-      <li class="{{ active('paket*') }}"><a class="nav-link" href="{{route('paket.index')}}"><i class="#"></i><span>Paket</span></a></li>           
+      <li class="{{ active('paket*') }}"><a class="nav-link" href="{{route('paket.index')}}"><i class="fa-solid fa-box"></i><span>Paket</span></a></li>           
+      
       <li class="menu-header">Transaksi</li>           
-      <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="#"></i><span>Transaksi</span></a></li>           
+      <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a></li>           
       <li class="menu-header">Laporan</li>           
-      <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="#"></i><span>Laporan</span></a></li>           
+      <li class="{{ active('detail_transaksi*') }}"><a class="nav-link" href="{{route('detail_transaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>           
     </ul>
   </aside>
 </div>
