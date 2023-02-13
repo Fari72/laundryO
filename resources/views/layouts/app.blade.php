413e05428a5dc0208237867b659f73c637ee5114
fari72 <fariaffrizal@gmail.com>
2023-02-13 10:29:42 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,7 @@
 
       <!-- Main Content -->
       <div class="main-content">
-        <section class="section">
-         @yield('content')
-        </section>
+        @yield('content')
       </div>
       @include('layouts.footer')
     </div>
