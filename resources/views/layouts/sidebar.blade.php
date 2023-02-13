d6f28ed82fccb09421bbb723562323746495958b
fari72 <fariaffrizal@gmail.com>
2023-02-13 09:19:02 +0700

    posisi sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,33 +8,24 @@
     </div> 
     <ul class="sidebar-menu">
       @if(auth()->user()->role == 'admin')
-      <li class="menu-header">Dashboard</li>
+      <li class="menu-header">Admin</li>
       <li class="{{ active('dashboard*') }}"><a class="nav-link" href="/dashboard"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>           
-      <li class="menu-header">User</li>
       <li class="{{ active('user*') }}"><a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-user"></i><span>User</span></a></li>
-      <li class="menu-header">Outlet</li>
-      <li class="{{ active('outlet*') }}"><a class="nav-link" href="{{route('outlet.index')}}"><i class="fa-solid fa-store"></i><span>Outlet</span></a></li>           
-      <li class="menu-header">Member</li>           
       <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="fa-solid fa-users"></i><span>Member</span></a></li>           
-      <li class="menu-header">Paket</li>           
+      <li class="{{ active('outlet*') }}"><a class="nav-link" href="{{route('outlet.index')}}"><i class="fa-solid fa-store"></i><span>Outlet</span></a></li>           
       <li class="{{ active('paket*') }}"><a class="nav-link" href="{{route('paket.index')}}"><i class="fa-solid fa-box"></i><span>Paket</span></a></li>           
-      <li class="menu-header">Transaksi</li>           
       <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a></li>           
-      <li class="menu-header">Laporan</li>           
       <li class="{{ active('detailtransaksi*') }}"><a class="nav-link" href="{{route('detailtransaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>           
       @endif
 
       @if(auth()->user()->role == 'kasir')
-      <li class="menu-header">Member</li>           
-      <li class="{{ active('member*') }}"><a class="nav-link" href="{{route('member.index')}}"><i class="fa-solid fa-users"></i><span>Member</span></a></li>
-      <li class="menu-header">Transaksi</li>           
+      <li class="menu-header">Kasir</li>           
       <li class="{{ active('transaksi*') }}"><a class="nav-link" href="{{route('transaksi.index')}}"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a></li>
-      <li class="menu-header">Laporan</li>           
       <li class="{{ active('detailtransaksi*') }}"><a class="nav-link" href="{{route('detailtransaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>
       @endif
 
       @if(auth()->user()->role == 'owner')
-      <li class="menu-header">Laporan</li>           
+      <li class="menu-header">owner</li>           
       <li class="{{ active('detailtransaksi*') }}"><a class="nav-link" href="{{route('detailtransaksi.index')}}"><i class="fa-solid fa-scroll"></i><span>Laporan</span></a></li>
       @endif
     </ul>
