1a07fee23d2c4bb5871fb4a1995ba3d27671deac
fari72 <fariaffrizal@gmail.com>
2023-02-13 09:59:28 +0700

    update icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,7 +101,7 @@
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
       <div class="card card-statistic-1">
         <div class="card-icon bg-danger">
-          <i class="fas fa-users"></i>
+          <i class="fas fa-store"></i>
         </div>
 
         <div class="card-wrap">
