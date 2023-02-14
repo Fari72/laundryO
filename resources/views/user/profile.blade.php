ddee96bbbee3f72cbf0490e506de9c40542a143c
fari72 <fariaffrizal@gmail.com>
2023-02-14 08:28:54 +0700

    update profile link

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,11 +4,13 @@
 
 @section('content')
   <div class="section-header">
-    <h1>Profile</h1>
-    <div class="section-header-breadcrumb">
+    <div class="card">
+      <h1>Profile</h1>
+    </div>
+    {{-- <div class="section-header-breadcrumb">
       <div class="breadcrumb-item active"><a href="/dashboard">Dashboard</a></div>
       <div class="breadcrumb-item">Profile</div>
-    </div>
+    </div> --}}
   </div>
 
   <div class="section-body">
